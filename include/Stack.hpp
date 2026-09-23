16c5a4f902e3dc2f16ae31b66dfb7ba31c33f44e
ruezomon <ruezgar.guenduez@gmail.com>
2026-09-23 19:20:22 +0200

    feat: roughly finish stack

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -7,10 +7,12 @@
 namespace dark {
     class Stack {
     private:
-        std::vector<dark::Card> cardstack;
-        dark::Card* cardTemplateArray;
+        std::vector<dark::Card>* cardstack = new std::vector<dark::Card>();
+        dark::Card* cardTemplateArray = new dark::Card[52];
     public:
         Stack() noexcept;
         void shuffle() noexcept;
+        dark::Card draw() noexcept;
+        bool canDraw() noexcept;
     };
 }
--- src/Stack.cpp
+++ src/Stack.cpp
@@ -1,5 +1,46 @@
+#include <random>
+
 #include "Stack.hpp"
 
 dark::Stack::Stack() noexcept {
-    
+    srand(time(NULL));
+
+    for (uint8_t i = 0; i < 4; i++) {
+        for (uint8_t j = 0; j < 13; j++) {
+            this->cardTemplateArray[i * 13 + j] = dark::Card(i, j);
+        }
+    }
+
+    this->shuffle();
+}
+
+void dark::Stack::shuffle() noexcept {
+    this->cardstack->clear();
+
+    uint8_t* used = new uint8_t[52]{0};
+    uint8_t currentIndex = 0;
+    bool foundUnique = false;
+    for (uint8_t i = 0; i < 52; i++) {
+        do {
+            currentIndex = rand() % 52;
+            for (int j = 0; j < 52; j++) {
+                if (used[j] == currentIndex) foundUnique = false;
+                break;
+            }
+        } while (!foundUnique);
+        this->cardstack->push_back(cardTemplateArray[currentIndex]);
+        used[i] = currentIndex;
+    }
+
+    delete[] used;
+}
+
+
+dark::Card dark::Stack::draw() noexcept {
+    if (!this->canDraw()) return dark::Card();
+    return this->cardstack->back();
+}
+
+bool dark::Stack::canDraw() noexcept {
+    return !this->cardstack->empty();
 }
